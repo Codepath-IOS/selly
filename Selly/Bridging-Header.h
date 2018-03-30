cc0aad72fd2a348d22880496c6b724a44b9afe85
Siddhant Patel <si12hant@gmail.com>
2018-03-30 18:59:41 -0400

    trying

---
+++ Selly/Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  Bridging-Header.h
+//  Selly
+//
+//  Created by siddhant on 3/30/18.
+//  Copyright © 2018 siddhant. All rights reserved.
+//
+#import <JSQMessagesViewController/JSQMessages.h>
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+// Include any system framework and library headers here that should be included in all compilation units.
+// You will also need to set the Prefix Header build setting of one or more of your targets to reference this file.
+
+#endif /* Bridging_Header_h */
